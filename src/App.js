aa258d76fc287026b4725ed0b4c68825b2d5420a
nromeron <nromeron@unal.edu.co>
2022-05-23 12:39:25 -0500

    merge

--- src/App.js
+++ src/App.js
@@ -12,36 +12,36 @@ import AuthProvider from "./providers/AuthProvider";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-      <Routes>
-        {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}>
-              {internalRoutes.map((internalRoute,index2) => (
-                  <Route
-                      key={index2}
-                      path={internalRoute.path}
-                      element = {<route.layout rol={route.path}>
-                          <internalRoute.component/>
-                      </route.layout>}
-                  />
-              ))}
-            </Route>
-        ))}
-        {visitorRoutes.map((visitorRoute,index3) => (
-            <Route
-              key={index3}
-              path={visitorRoute.path}
-              element = {<visitorRoute.layout side={visitorRoute.path === "/"}>
-                  <visitorRoute.component/>
-              </visitorRoute.layout>}
-            />
-        ))}
-      </Routes>
-    </BrowserRouter>
-    </AuthProvider>
+      <AuthProvider>
+          <BrowserRouter>
+          <Routes>
+                {routes.map((route, index) => (
+                    <Route
+                      key={index}
+                      path={route.path}>
+                      {internalRoutes.map((internalRoute,index2) => (
+                          <Route
+                              key={index2}
+                              path={internalRoute.path}
+                              element = {<route.layout rol={route.path}>
+                                  <internalRoute.component/>
+                              </route.layout>}
+                          />
+                      ))}
+                    </Route>
+                ))}
+            {visitorRoutes.map((visitorRoute,index3) => (
+                <Route
+                  key={index3}
+                  path={visitorRoute.path}
+                  element = {<visitorRoute.layout side={visitorRoute.path === "/"}>
+                      <visitorRoute.component/>
+                  </visitorRoute.layout>}
+                />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
   );
 }
 
