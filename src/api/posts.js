e4c8c730c3a79d2d44deeec278e01b09ed0f66a8
JuanPabloBustamante <jubustamantem@unal.edu.co>
2022-05-26 02:20:01 -0500

    Anuncios

---
+++ src/api/posts.js
@@ -0,0 +1,18 @@
+export function anuncia(data,id){
+    data.freelancerId = id
+    const url = 'http://localhost:8000/post/create';
+    const params = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    return fetch(url, params).then(response =>{
+        return response.json();
+    })
+        .then(result => {
+            return result;
+        })
+}
--- src/views/Anuncios.jsx
+++ src/views/Anuncios.jsx
@@ -1,65 +1,119 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {notification} from "antd";
+import {anuncia} from "../api/posts";
+import{getAccessToken} from "../api/auth"
+import jwtDecode from "jwt-decode";
 
 const Anuncios = () => {
+
+  const user = jwtDecode(getAccessToken())
+
+  const [inputs, setInputs] = useState({
+    postTitle: "",
+    freelancerId: "",
+    postDescription: "",
+    postPrice: "",
+    postCategory: 1,
+    thumbnailUrl: "",
+    adPriority: 0
+  });
+
+  const changeForm = e => {
+      setInputs({
+        ...inputs,
+        [e.target.name]: e.target.value
+      });
+  };
+
+  const publish = async e => {
+    e.preventDefault();
+    const result = await anuncia(inputs,user.sub.id);
+    console.log(result)
+    if (!result.id) {
+      notification["error"]({
+        message: result.message
+      });
+    } else {
+        resetForm();
+        window.location.href = "/";
+    }
+  }
+
+  const resetForm = () => {
+    const inputs = document.getElementsByTagName("input");
+
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].classList.remove("success");
+      inputs[i].classList.remove("error");
+    }
+
+    setInputs({
+      postTitle: "",
+      freelancerId: "",
+      postDescription: "",
+      postPrice: "",
+      postCategory: 1,
+      thumbnailUrl: "",
+      adPriority: 0
+    });
+  };
   return (
       <div className="contenedorPerfil text-center d-flex">
         <div className="container rounded row">
+          <h5 className=" welcome mb-3 fw-bold">Anunciarme</h5>
+          <div onSubmit={publish} onChange={changeForm}>
           <form>
-            <div className="row">
-              <div className="col-xl-3 col-md-6 mb-4">
-                <div className="card border-left-primary h-100 py-2">
-                  <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                      <div className="col-auto">
-                        <i className="bi bi-camera"></i>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-xl-3 col-md-6 mb-10">
-                <div className="card border-left-success  h-100 py-2">
-                  <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                      <div className="col mr-2">
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">
-                          Nombre del producto o servicio
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-xl-3 col-md-10 mb-4">
-                <div className="card border-left-warning  h-80 py-7">
-                  <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                      <div className="col mr-2">
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">
-                          $ Precio
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
             <div className="row mt-3">
-              <div className="form-floating col-lg-20">
+              <div className="form-floating col">
                 <input
                     type="text"
                     className="form-control mb-3"
-                    id="dir"
-                    name="dir"
-                    placeholder="dir"
+                    id="postCategory"
+                    name="postCategory"
+                    placeholder="postCategory"
+                    value = {inputs.postCategory}
+                    readOnly
                 />
-                <label htmlFor="password" className="ms-3">¿En qué consiste el producto o servicio que deseas ofrecer?</label>
+                <label htmlFor="PostCategoryId" className="ms-3">Categoría</label>
               </div>
+              <div className="form-floating col">
+                <input
+                    type="text"
+                    className="form-control mb-3"
+                    id="postTitle"
+                    name="postTitle"
+                    placeholder="postTitle"
+                    value = {inputs.postTitle}
+                />
+                <label htmlFor="postTitle" className="ms-3">Título</label>
+              </div>
+            <div className="form-floating col">
+              <input
+                  type="text"
+                  className="form-control mb-3"
+                  id="postPrice"
+                  name="postPrice"
+                  placeholder="postPrice"
+                  value = {inputs.postPrice}
+              />
+              <label htmlFor="postPrice" className="ms-3">Precio</label>
             </div>
+            </div>
+              <div className="form-floating col">
+                <textarea
+                    className="form-control mb-3 h-100"
+                    id="postDescription"
+                    name="postDescription"
+                    placeholder="postDescription"
+                    value = {inputs.postDescription}
+                />
+                <label htmlFor="postDescription" className="ms-3">¿En qué consiste el producto o servicio que deseas ofrecer?</label>
+              </div>
             <div className="col">
               <button className="w-70 btn btn-lg btn-primary fw-bold" type="submit">Publicar</button>
             </div>
           </form>
+          </div>
         </div>
       </div>
   )
